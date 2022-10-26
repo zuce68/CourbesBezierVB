d2b3510484a0943551f612fddec3928bf6eab941
pmisiuk <pmisiuk@A404PC11>
2022-10-26 08:37:20 +0200

    Ajoutez des fichiers projet.

---
+++ CourbesBezier/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de
+' l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+' associées à un assembly.
+
+' Vérifiez les valeurs des attributs de l'assembly
+
+<Assembly: AssemblyTitle("CourbesBezier")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Unistra")>
+<Assembly: AssemblyProduct("CourbesBezier")>
+<Assembly: AssemblyCopyright("Copyright © Unistra 2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("f53e6ec8-05b4-4fac-b385-e197f505a31f")>
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Version principale
+'      Version secondaire
+'      Numéro de build
+'      Révision
+'
+' Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+' en utilisant '*', comme indiqué ci-dessous :
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
